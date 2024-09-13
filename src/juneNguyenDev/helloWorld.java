40ac06cdd7ec61aae305578e552ddbbec64b7fda
juneNguyenDev <junenene2906@gmail.com>
2024-09-13 15:52:01 +0700

    java_hello

---
+++ src/juneNguyenDev/helloWorld.java
@@ -0,0 +1,9 @@
+package juneNguyenDev;
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world 1");
+		System.out.println("Hello world 2");
+	}
+}
